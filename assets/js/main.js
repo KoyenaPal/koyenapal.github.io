9bd840132dbda14e19fdba8bc3f160ac1666fdfe
Koyena Pal <koyenapal12@gmail.com>
2022-01-01 23:43:32 +0300

    updated Dec news

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 var news_entries = [
+    ["Dec 2021",`Taught AI and Ethics to high school students at <a style="color: blue" target="_blank" class="link" href="https://www.inspiritai.com/">Inspirit AI</a> Winter Session program.`],
     ["Sept 2021", `Joined <a style="color: #da532c" target="_blank" class="link" href="https://www.brown.edu/">Brown University</a> as a Master's graduate student`],
     ["July 2021", `Awarded Brown CS Scholarship to attend <a style="color: blue" href="https://tapiaconference.cmd-it.org/" target="_blank">2021 Richard Tapia Conference</a>.`],
     ["May 2021", `Graduated from <a style="color: #da532c" target="_blank" class="link" href="https://www.brown.edu/">Brown University</a> with B.Sc in Computer Science (Honors)!`],
     ["April 2021", `Successfully defended my <a style="color: blue" target="_blank" class="link" href="http://cs.brown.edu/research/pubs/theses/ugrad/2021/pal.koyena.pdf">undergraduate thesis</a>!`]
 ]
-var curr_news_cutoff = 4
+var curr_news_cutoff = 5
 
 function display_recent_news() {
     var news_entries_text = ``
